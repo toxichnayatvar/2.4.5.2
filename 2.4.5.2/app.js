620949a10595e7bab018029992c3bc7372f47f78
toxichnayatvar <123656439+toxichnayatvar@users.noreply.github.com>
2023-02-20 00:41:15 +0300

    Add files via upload

---
+++ 2.4.5.2/app.js
@@ -0,0 +1,12 @@
+function f() {
+    alert(`Будет ${2+2+1}`)
+}
+
+function g() {
+    let name = prompt('Кто ты из феечек винкс?')
+    alert(`Из феечек винкс ты ${name}`)
+}
+
+function semen() {
+    document.getElementById('ncb').innerText = 'ДА'
+}
